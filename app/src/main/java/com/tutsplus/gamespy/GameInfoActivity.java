feb2b28a308eb6831bcbf75a4a3ef5971bcf4d5f
Alex Wong <alexwong11611@gmail.com>
2017-12-18 20:38:10 +0000

    Third commit, final commit, touched up UI. changed text to strings. Added spanish language.

--- app/src/main/java/com/tutsplus/gamespy/GameInfoActivity.java
+++ app/src/main/java/com/tutsplus/gamespy/GameInfoActivity.java
@@ -26,16 +26,15 @@ public class GameInfoActivity extends AppCompatActivity {
     public void getContentBasedOnTitle(String title) {
         String textToShow;
         switch (title) {
-            case "DARK SOULS\n":  textToShow = "Dark souls is an action RPG made by From software and is widely claimed to be one of the toughest games out there on the market";
+            case "DARK SOULS\n":  textToShow = getString(R.string.darkSoul);
                 break;
-            case "FORTNITE\n":  textToShow = "Fortnite is a game developed by Epic games that is a battle royal style game currently free to play and a PvE game that is purchasable";
+            case "FORTNITE\n":  textToShow = getString(R.string.fortnite);
                 break;
-            case "SKYRIM\n":  textToShow = "Skyrim is the fifth elder scrolls game developed by Bethesda studios. A game that has made its claim to fame through the development of Elder scrolls and Skyrim games. ";
+            case "SKYRIM\n":  textToShow = getString(R.string.skyRim);
                 break;
-            case "SUPER MARIO\n":  textToShow = "Super mario is a long string of games of the Mario series made by nintendo, spanning across several consoles and widely reveared as one of the great kings of videogames.";
+            case "SUPER\nMARIO\nTM\n":  textToShow = getString(R.string.superMario);
                 break;
-            case "CLASH OF\nCLANS\n": textToShow = "Clash of Clans is a freemium mobile strategy video game developed and published by Finnish game developer Supercell. The game was released for iOS platforms on August 2, 2012, and on Google Play for Android on October 7, 2013.\n" +
-                    "The game is set in a fantasy themed persistent world where the player is a chief of a village. Clash of Clans tasks players to build their own town using the resources gained from attacking other players through the game's fighting features. The main resources are gold, elixir and dark elixir. Players can conjoin to create clans, groups of up to fifty people who can then partake in Clan Wars together, donate and receive troops, and talk with each other.";
+            case "CLASH OF\nCLANS\n": textToShow = getString(R.string.clashOfClan);
                 break;
             default: textToShow = "Invalid title";
                 break;
